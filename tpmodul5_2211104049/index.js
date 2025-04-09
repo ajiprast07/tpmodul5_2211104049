73d7c636bced8c7364d9ec8b18e167986f482c59
ajiprast07 <ajiprasetyonugroho123@gmail.com>
2025-04-09 23:27:10 +0700

    commit

---
+++ tpmodul5_2211104049/index.js
@@ -0,0 +1,13 @@
+//1
+const HaloGeneric = require('./haloGeneric');
+
+const halo = new HaloGeneric();
+halo.SapaUser("Aji");
+
+
+//2
+const DataGeneric = require('./dataGeneric');
+
+const data = new DataGeneric("2211104049");
+data.PrintData();
+
